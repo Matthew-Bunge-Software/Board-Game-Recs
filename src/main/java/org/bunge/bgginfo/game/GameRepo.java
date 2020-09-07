7a0ab7827dfa92541e224325feda5d04d0edf417
Matthew Bunge <10692492+mattb555@users.noreply.github.com>
2020-09-07 02:45:00 -0700

    Updated repo call (#16)

--- src/main/java/org/bunge/bgginfo/game/GameRepo.java
+++ src/main/java/org/bunge/bgginfo/game/GameRepo.java
@@ -9,7 +9,14 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface GameRepo extends CrudRepository<GameDAO, Integer> {
     
-    @Query(value = "SELECT `name`, average, num_ratings, weight FROM game CROSS JOIN (SELECT MAX(average) AS average_max FROM game) AS maxAverageTable CROSS JOIN (SELECT MAX(num_ratings) AS num_max FROM game) AS maxNumTable WHERE error = false AND num_ratings > 4 ORDER BY ((average / average_max) * ?1 + (num_ratings / num_max) * (1 - ?1)) DESC LIMIT 500;",
+    @Query(value = "SELECT `name`, average, num_ratings, weight " +
+                    "FROM game " + 
+                    "CROSS JOIN (" +
+                        "SELECT MAX(average) AS average_max FROM game) AS maxAverageTable " +
+                    "CROSS JOIN ( " +
+                        "SELECT MAX(num_ratings) AS num_max FROM game) AS maxNumTable " +
+                    "WHERE error = false AND num_ratings > 4 " + 
+                    "ORDER BY ((average / (average_max * 1.0)) * ?1 + (num_ratings / (num_max * 1.0)) * (1.0 - ?1)) DESC LIMIT 500;",
         nativeQuery = true)
     List<GameDTO> findByAverage(Double average);
 }
